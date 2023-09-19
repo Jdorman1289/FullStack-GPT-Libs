ebc825d66193b00d07ccabc1e22c0da6fb34f00b
Jesse <Jdorman1289@gmail.com>
2023-09-19 11:53:41 -0500

    update seeder file

--- src/main/seeder.sql
+++ src/main/seeder.sql
@@ -1,4 +1,6 @@
 INSERT INTO mad_lib (adj, adv, noun, story, verb)
-VALUES ('funny', 'quickly', 'dog', 'The {{adj}} {{noun}} {{verb}} {{adv}} over the lazy brown fox.', 'jumped'),
-       ('big', 'slowly', 'cat', 'The {{adj}} {{noun}} {{verb}} {{adv}} over the lazy brown dog.', 'walked'),
-       ('happy', 'loudly', 'bird', 'The {{adj}} {{noun}} {{verb}} {{adv}} over the lazy brown cat.', 'sang');
+VALUES ('funny', 'quickly', 'dog', 'The funny dog jumped quickly over the lazy brown fox.', 'jumped'),
+       ('big', 'slowly', 'cat', 'The big cat walked slowly over the lazy brown dog.', 'walked'),
+       ('happy', 'loudly', 'bird', 'The happy bird sang loudly over the lazy brown cat.', 'sang');
+
+
