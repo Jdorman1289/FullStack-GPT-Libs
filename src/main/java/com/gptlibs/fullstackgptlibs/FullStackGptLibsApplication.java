1d7a6c9ff01e201ebe9dac022748ce234a2e7d62
Jesse Dorman <Jdorman1289@gmail.com>
2023-09-18 19:03:39 -0500

    First push / create the bones for Spring

---
+++ src/main/java/com/gptlibs/fullstackgptlibs/FullStackGptLibsApplication.java
@@ -0,0 +1,13 @@
+package com.gptlibs.fullstackgptlibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackGptLibsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackGptLibsApplication.class, args);
+    }
+
+}
