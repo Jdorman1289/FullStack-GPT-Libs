173ad0f2c5ac826d88b192ca3567116589ede52f
Jesse <Jdorman1289@gmail.com>
2023-09-19 11:16:54 -0500

    add a seeder file for style testing without making an API call every time

---
+++ src/main/seeder.sql
@@ -0,0 +1,4 @@
+INSERT INTO mad_lib (adj, adv, noun, story, verb)
+VALUES ('funny', 'quickly', 'dog', 'The {{adj}} {{noun}} {{verb}} {{adv}} over the lazy brown fox.', 'jumped'),
+       ('big', 'slowly', 'cat', 'The {{adj}} {{noun}} {{verb}} {{adv}} over the lazy brown dog.', 'walked'),
+       ('happy', 'loudly', 'bird', 'The {{adj}} {{noun}} {{verb}} {{adv}} over the lazy brown cat.', 'sang');
